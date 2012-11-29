d6b2a7e2b3c234a9153d6deb2feae7eb3e86ecdf
Meiring <jmeiring@A-POWERSMBP01.local>
2012-11-29 13:55:43 -0600

    First commit

---
+++ FlaskExample/views.py
@@ -0,0 +1,19 @@
+from FlaskExample import app
+from flask import (request, url_for, redirect, render_template, flash,
+                   session, g, json, Blueprint, make_response, Flask)
+
+
+@app.route('/', methods=['GET','POST'])
+def index():
+	if request.method == 'GET':
+		print "GET happened"
+		return render_template('index.html')
+	elif request.method == 'POST':
+		print "POST happened"
+		print request.form
+		return render_template('index.html')
+
+
+
+
+    
---
+++ runserver.py
@@ -0,0 +1,2 @@
+from FlaskExample import app
+app.run(debug=True)
